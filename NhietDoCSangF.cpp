a59613b554e18605fad2e9fb48f6e406054028ea
Quater <102786860+quanganh296@users.noreply.github.com>
2024-12-11 10:48:42 +0700

    Add files via upload

---
+++ NhietDoCSangF.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<stdio.h> 
+#include <math.h>
+using namespace std;
+int main()
+{
+	int C;
+	printf("Hay nhap vao nhiet do C: "); 
+	scanf("%d",&C);
+	float F=0;
+	F = C*1.8+32;
+	printf("Nhiet do F là: %.2f  do \n",F); 
+	
+	
+	return 0;
+}
